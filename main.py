a62db7ac529f2b658c0e6c9590cb1dc062e1234c
LyshaJaufar <lysha.ahmd@gmail.com>
2021-06-11 20:32:10 +0500

    upd

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
-print("hello")
-print("world")
+from sys import argv, exit
+import csv
+
+
+# Check for command-line args
+if len(argv) != 2:
+    print("Error")
+    exit(1)
+
+# Open the csv file
+database = open(argv[1], "r")
+if (database == None):
+    exit(2)
+
+
+# Answer Sheet
+file = open('Income Statement Ans.csv', 'w', newline="")
+if (file == None):
+    exit(3)
+
+csvReader = csv.DictReader(database)
+fieldnames = ['Details','1','2','3']
+csvWriter = csv.DictWriter(file, fieldnames=fieldnames)
+
+csvWriter.writeheader()
+
+# Prompt for FC, VC and price
+closedInventory = int(input("Enter closed inventory: "))
+
+
+for row in csvReader:
+    # Variables for each 
+    row['details'].lower == "Revenue"
+    # Calculate fixed cost
+    if row['1'].lower() == "":
+        if row['details'].lower == "Revenue":
+            revenue = 90
+        print("yes")
+        row['1'] = 3
+
+    csvWriter.writerow(row)
+
+
+# Close files
+database.close()
+file.close()
